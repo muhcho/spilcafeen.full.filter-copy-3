02fa8cde3060ff1175888cb4b7677e2b74228115
muhcho <144784372+muhcho@users.noreply.github.com>
2024-09-05 15:00:08 +0200

    h

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+      plugins: [react()],
+      base: "/"
+  };
+
+  if (command !== "serve") {
+      config.base = "/spilcafeen.full.filter-copy-3/";
+  }
+
+  return config;
+});
